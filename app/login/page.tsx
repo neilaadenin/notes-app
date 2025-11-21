84e71b10f74e1187b9685b92b36b6f01d3a26408
neilaadenin <neilaadeninsyafitri@gmail.com>
2025-11-21 14:11:52 +0700

    revisi login

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-export default function SignUpPage() {
+export default function LoginPage() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[#f7f8fc] px-4">
-      <h1 className="text-3xl font-semibold text-[#5a5eb9] mb-1">Sign up</h1>
+      <h1 className="text-3xl font-semibold text-[#5a5eb9] mb-1">Log in</h1>
       <p className="text-gray-500 mb-6">Join the community today!</p>
 
       <button className="flex items-center gap-2 w-full max-w-md bg-white py-3 rounded-full shadow text-gray-700 justify-center mb-4">
@@ -39,13 +39,11 @@ export default function SignUpPage() {
           type="submit"
           className="w-full bg-gradient-to-r from-[#7d8cf3] to-[#5a5eb9] text-white py-3 rounded-full mt-2"
         >
-          Sign up
+          Log in
         </button>
       </form>
 
-      <p className="text-gray-600 mt-6">
-        Already a member? <a className="text-[#5a5eb9]">Sign in</a>
-      </p>
+      
     </div>
   );
 }
